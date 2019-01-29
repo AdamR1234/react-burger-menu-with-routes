f3b494e8be1a0e4197f1e6a47577a81584e8ebb2
AdamR1234 <adam-@msn.com>
2019-01-29 16:04:31 +0000

    Style and info updates
    Updates to the styles, colours and content

--- src/home.js
+++ src/home.js
@@ -6,10 +6,36 @@ class Home extends Component {
       <div class="container">
         <div class="row">
           <div class="col-sm-12">
-            <h1>Adam Roe</h1>
+            <h1>A|R</h1>
           </div>
-          <div class="col-sm-3">One of three columns</div>
-          <div class="col-sm-3">One of three columns</div>
+        </div>
+
+        <div class="row">
+          <div
+            class="col-sm-3 mr-3 ml-3"
+            style={{ background: "rgb(142, 184, 222)" }}
+          >
+            <h4>Who?</h4>
+
+            <p class="lead">
+              <strong>Adam Roe</strong>
+              <small> BSc (Hons)</small>
+            </p>
+
+            <p class="block-quote">
+              I'm a 2:1 graduate in computing from the University of Central
+              Lancashire.
+            </p>
+
+            <p class="block-quote">
+              With a passion for web development, I'm always experimenting with
+              new styles and learning modern technologies/practices.
+            </p>
+          </div>
+
+          <div class="col-sm-3 mr-3 ml-3" style={{ background: "#4cd646" }}>
+            <h4>Why?</h4>
+          </div>
         </div>
       </div>
     );
